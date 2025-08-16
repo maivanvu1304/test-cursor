e95bf5692f977c9c9693931edd6ab166b7352f04
maivanvu1304 <maivanvu134@gmail.com>
2025-08-16 14:31:29 +0700

    first commit

---
+++ test.py
@@ -0,0 +1,4 @@
+import pprint
+import requests
+
+pprint.pprint(requests.get('https://gist.githubusercontent.com/emarco177/859ec7d786b45d8e3e3f688c6c9139d8/raw/5eaf8e46dc29a98612c8fe0c774123a7a2ac4575/eden-marco-scrapin.json').json())
